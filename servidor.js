452a612f00263db2771a5117c56a9311179ec3de
unknown <braian.bilicic@gmail.com>
2022-03-05 08:47:28 -0300

    Ultimos retoques en todos los get

--- servidor.js
+++ servidor.js
@@ -8,10 +8,13 @@ const server = app.listen(PORT, () => {
 server.on('error', error => console.log(`Error in server ${error}`));
 
 
+//utilizar la clase Contenedor del desafío anterior para acceder a los datos persistidos del servidor.
 const modulo = require("./container.js")
-
-
 const container = new modulo.Container("./productos.txt")
+
+
+//4 productos de prueba para este ejercicio
+//Antes de iniciar el servidor, colocar en el archivo 'productos.txt' tres productos como en el ejemplo del desafío anterior.
 
 let objetoPrueba = {
     title: "Pinza Bremen",
@@ -38,58 +41,53 @@ let objetoPrueba4 = {
 }
 
 
-
- 
-
-
+// Ruta get '/productos' que devuelva un array con todos los productos  
+// disponibles en el servidor
 app.get('/products', (req, res) => {
-    async function ejecutar() {
+    async function productsFunction() {
         res.send(await container.getAll());
     }
-    ejecutar();
+    productsFunction();
 })
 
-
-
-
+// Ruta get '/productoRandom' que devuelva un producto elegido al azar 
+// entre todos los productos disponibles
 app.get('/productRandom', (req, res) => {
-
-    async function test() {
+    async function productRandomFunction() {
         const arrayObjets = await container.getAll()
         const min = 1;
         const max = arrayObjets.length;
         const random = Math.floor(Math.random() * (max - min)) + min;
         res.send(await container.getById(random));
     }
-    
-    test();
+    productRandomFunction();
 }
 )
 
-async function saveProductsTest() {
-
+//Funcion async del save para no poner todo adentro del get
+async function saveProducts() {
     await container.save(objetoPrueba);
     await container.save(objetoPrueba2);
     await container.save(objetoPrueba3);
     await container.save(objetoPrueba4);
-    
 }
 
-
+//Este metodo get es para guardar productos y asi probar que funcionen los metodos de la clase contenedor 
 app.get('/save',  (req, res) => {
-    async function saveProducts() {
-        await saveProductsTest();
-        res.send('Guardado');
+    async function saveFunction() {
+        await saveProducts();
+        res.send('Saved');
     }
-    saveProducts();
+    saveFunction();
 })
 
+//Este metodo get los elimina y deja el contenedor vacio
 app.get('/delete', (req, res) => {
-    async function ejecutar() {
+    async function deleteFunction() {
         await container.deleteAll();
-        res.send('Borrado');
+        res.send('Delete');
     }
-    ejecutar();
+    deleteFunction();
    
 })
 
